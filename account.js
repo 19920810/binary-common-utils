d3892bd257d19aa8e25e774bac8a9ea46131ea69
Amin Marashi <marashi.s.amin@gmail.com>
2016-07-18 09:18:27 +0800

    Updated account.js

--- account.js
+++ account.js
@@ -3,20 +3,22 @@ var storageManager = require('./storageManager');
 
 module.exports = {
 	addTokenIfValid: function addTokenIfValid(token, callback) {
-		var api = new CustomApi()._originalApi;
+		var LiveApi = require('binary-live-api')
+			.LiveApi;
+		var api = new LiveApi();
 		api.authorize(token)
 			.then(function (response) {
 				api.disconnect();
-				storageManager.addToken(token, response.authorize.loginid);
+				storageManager.addToken(token, response.authorize.loginid, response.authorize.is_virtual);
 				if (callback) {
 					callback(null);
 				}
 			}, function (reason) {
 				api.disconnect();
-				storageManager.removeToken(token);
+				removeToken(token);
 				if (callback) {
 					callback('Error');
 				}
 			});
-	},
+	}
 };
