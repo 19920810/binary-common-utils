e7af8e9ed2c27ca0d1d6e01f129e82e50bba8cc3
Amin Marashi <marashi.s.amin@gmail.com>
2016-08-22 12:01:09 +0800

    Reduced version of binary-live-api, fixed tests

--- __tests__/tools.js
+++ __tests__/tools.js
@@ -50,61 +50,11 @@ describe('Misc Tools', function(){
 			expect(tools.getObjectValue(obj)).to.be.equal('value');
 		});
 	});
-	describe('getObjectKey', function(){
-		it('getObjectKey gets the key of an object with one attribute', function(){
-			var obj = {key: 'value'};
-			expect(tools.getObjectKey(obj)).to.be.equal('key');
-		});
-	});
-	describe('copyAttributeIfExists', function(){
-		it('copyAttributeIfExists copies attribute of one object to another', function(){
-			var obj1 = {key: 'value'};
-			var obj2 = {key2: 'value2'};
-			tools.copyAttributeIfExists(obj1, obj2, 'key2');
-			expect(obj1.key2).to.be.ok;
-		});
-	});
 	describe('getUTCTime', function(){
 		it('getUTCTime gets the current UTC time with format HH:MM:SS', function(){
 			var date = new Date('1990-01-01T01:11:10.000Z');
 			expect(tools.getUTCTime(date)).to.be.equal('01:11:10');
 		});
-	});
-});
-describe('Xml Tools', function(){
-	var markets = {};
-	var marketsXml = null;
-	var marketsXmlStr = '';
-	before(function(){
-		markets = {
-			random: {
-				name: 'Random',
-				submarkets: {
-					indices: {
-						name: 'indices',
-						symbols: {
-							r_100: {
-								display: 'Random 100'
-							}
-						}
-					}
-				}
-			}
-		};
-	});
-	it('createXmlFromMarket should convert market to xml string', function(){
-		marketsXmlStr = tools.createXmlFromMarket(markets);
-		expect(marketsXmlStr).to.be.a('string');
-	});
-	it('strToXml should convert market to xml', function(){
-		marketsXml = tools.strToXml(marketsXmlStr);
-		expect(marketsXml).to.have.property('childNodes')
-		.that.has.property('0')
-		.that.satisfy(function(dom){return dom.getAttribute('name') === 'Markets';});
-	});
-	it('xmlToStr should convert market xml back to string', function(){
-		var newMarketStr = tools.xmlToStr(marketsXml);
-		expect(newMarketStr).to.be.equal(marketsXmlStr);
 	});
 	after(function(){
 		observer._destroy();
