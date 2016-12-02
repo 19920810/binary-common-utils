28664ff0b7ddda4cb5cec268ce793af93d3ee6a2
Amin Marashi <marashi.s.amin@gmail.com>
2016-12-02 10:34:27 +0800

    Removed xmldom

--- src/mochaHelper.js
+++ src/mochaHelper.js
@@ -1,7 +1,4 @@
 require('babel-register')();
 require('jsdom-global')();
-const xmldom = require('xmldom');
 
 global.localStorage = {};
-global.XMLSerializer = xmldom.XMLSerializer;
-global.DOMParser = xmldom.DOMParser;
