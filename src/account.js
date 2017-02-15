f98c797e6c6294a6b5229f8a70686b00b3a59aa8
Amin Marashi <marashi.s.amin@gmail.com>
2017-02-15 10:03:03 +0800

    Add appId to account api calls

--- src/account.js
+++ src/account.js
@@ -1,34 +1,38 @@
 import { LiveApi } from 'binary-live-api';
-import { addToken, removeToken, getTokenList, removeAllTokens } from './storageManager';
+import {
+  addToken, removeToken, getTokenList, removeAllTokens,
+  get as getStorage,
+} from './storageManager';
+
+
+const options = {
+  websocket: typeof WebSocket === 'undefined' ? require('ws') : undefined, // eslint-disable-line global-require
+  language: getStorage('lang') || 'en',
+  appId: getStorage('appId') || 1,
+};
 
 export const addTokenIfValid = (token, callback = () => {
   }) => {
-  const option = (typeof WebSocket === 'undefined') ? {
-    websocket: require('ws'), // eslint-disable-line global-require, import/no-extraneous-dependencies
-  } : {}; // eslint-disable-line import/newline-after-import
-  const api = new LiveApi(option);
-  api.authorize(token)
-    .then((response) => {
-      const landingCompanyName = response.authorize.landing_company_name;
-      api.getLandingCompanyDetails(landingCompanyName).then(r => {
-        addToken(token, response.authorize.loginid,
-          !!response.authorize.is_virtual, !!r.landing_company_details.has_reality_check,
-          ['iom', 'malta'].includes(landingCompanyName));
+    const api = new LiveApi(options);
+    api.authorize(token)
+      .then((response) => {
+        const landingCompanyName = response.authorize.landing_company_name;
+        api.getLandingCompanyDetails(landingCompanyName).then(r => {
+          addToken(token, response.authorize.loginid,
+            !!response.authorize.is_virtual, !!r.landing_company_details.has_reality_check,
+            ['iom', 'malta'].includes(landingCompanyName));
+          api.disconnect();
+          callback(null);
+        }, () => 0);
+      }, () => {
+        removeToken(token);
         api.disconnect();
-        callback(null);
-      }, () => 0);
-    }, () => {
-      removeToken(token);
-      api.disconnect();
-      callback('Error');
-    });
+        callback('Error');
+      });
 };
 
 export const logoutAllTokens = (callback) => {
-  const option = (typeof WebSocket === 'undefined') ? {
-      websocket: require('ws'), // eslint-disable-line global-require, import/no-extraneous-dependencies
-    } : {}; // eslint-disable-line import/newline-after-import
-  const api = new LiveApi(option);
+  const api = new LiveApi(options);
   const tokenList = getTokenList();
   const logout = () => {
     removeAllTokens();
