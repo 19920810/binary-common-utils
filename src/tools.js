eebd95374d81ddd63e6c669c1b71edb95465c70d
Amin Marashi <marashi.s.amin@gmail.com>
2017-07-11 10:13:14 +0800

    Add durationToSecond

--- src/tools.js
+++ src/tools.js
@@ -4,20 +4,46 @@ export const parseQueryString = () => {
   }
   const str = window.location.search;
   const objURL = {};
-  str.replace(
-    new RegExp('([^?=&]+)(=([^&]*))?', 'g'),
-    (a0, a1, a2, a3) => {
-      objURL[a1] = a3;
-    });
+  str.replace(new RegExp('([^?=&]+)(=([^&]*))?', 'g'), (a0, a1, a2, a3) => {
+    objURL[a1] = a3;
+  });
   return objURL;
 };
 
-export const getObjectValue = (obj) => obj[Object.keys(obj)[0]];
+export const getObjectValue = obj => obj[Object.keys(obj)[0]];
 
-export const getUTCTime = (date) => {
+export const getUTCTime = date => {
   const dateObject = new Date(date);
-  return ('0' + dateObject.getUTCHours())
-      .slice(-2) + ':' + ('0' + dateObject.getUTCMinutes())
-      .slice(-2) + ':' + ('0' + dateObject.getUTCSeconds())
-      .slice(-2);
+  return (
+    ('0' + dateObject.getUTCHours()).slice(-2) +
+    ':' +
+    ('0' + dateObject.getUTCMinutes()).slice(-2) +
+    ':' +
+    ('0' + dateObject.getUTCSeconds()).slice(-2)
+  );
+};
+
+export const durationToSecond = duration => {
+  const parsedDuration = duration.match(/^([0-9]+)([stmhd])$/);
+  if (!parsedDuration) {
+    return null;
+  }
+  const durationValue = parseFloat(parsedDuration[1]);
+  const durationType = parsedDuration[2];
+  if (durationType === 's') {
+    return durationValue;
+  }
+  if (durationType === 't') {
+    return durationValue * 2;
+  }
+  if (durationType === 'm') {
+    return durationValue * 60;
+  }
+  if (durationType === 'h') {
+    return durationValue * 60 * 60;
+  }
+  if (durationType === 'd') {
+    return durationValue * 60 * 60 * 24;
+  }
+  return 0;
 };
